2073358564a714d729eb1ced9c87cebfc2d1d7b7
LazySelkie <54956262+LazySelkie@users.noreply.github.com>
2024-04-27 23:51:50 +0300

    test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import { createApp } from "vue";
+import { createWebHistory, createRouter } from 'vue-router'
 import App from './App.vue'     //Our .vue startup file
 
+// import HomeView from './HomeView.vue' // импортим так то, на что ссылки
+import MainView from './views/MainView.vue';
+import EquipmentView from './views/EquipmentView.vue';
+import NotFound from './views/NotFound.vue';
+const routes = [
+    { path: '/', component: MainView },
+    { path: '/equipment', component: EquipmentView },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+];
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
 //Will mount the vue app inside a HTML element which id equals to "app" (div into templetes/index.html file)
-createApp(App).mount('#app');
+createApp(App)
+    .use(router)
+    .mount('#app');
 
 import './static/global.css'    //Import css files as modules
---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Not found. Please proceed to <RouterLink to="/">main page</RouterLink></h1>
+</template>
+
+<script>
+
+import { defineComponent } from 'vue'; 
+
+export default defineComponent({
+    name: 'NotFound'
+});
+
+</script>
