dbde69128f6dbc6a14d88d1380b453b3fa711c18
LazySelkie <54956262+LazySelkie@users.noreply.github.com>
2024-04-28 02:36:46 +0300

    router fix 2

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ import MainView from './views/MainView.vue';
 import EquipmentView from './views/EquipmentView.vue';
 import NotFound from './views/NotFound.vue';
 const routes = [
-    { path: '/irrilium/', component: MainView },
+    { path: '/', redirect: { name: 'Home' } },
+    { path: '/irrilium/', name: 'Home', component: MainView },
     { path: '/irrilium/equipment', component: EquipmentView },
     { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ];
