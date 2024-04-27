7ab7092ab931819caaf3655d5db9ca30a135812d
LazySelkie <54956262+LazySelkie@users.noreply.github.com>
2024-04-27 20:04:27 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Minimal Vue Webpack</h1>
+</template>
+
+<script>
+
+import { defineComponent } from 'vue'; 
+
+export default defineComponent({
+    name: 'App'
+});
+
+</script>
+
+<style scoped>
+    h1 {
+        width: 100%;
+        text-align: center;
+        margin-top: 10px;
+    }
+</style>
